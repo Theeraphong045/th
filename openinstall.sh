c9f6759350cf3559e8db514fb3300b8547607233
Theeraphong045 <31796274+Theeraphong045@users.noreply.github.com>
2017-09-14 16:13:23 +0700

    Update openinstall.sh

--- openinstall.sh
+++ openinstall.sh
@@ -20,7 +20,14 @@ apt-get update
 apt-get install pritunl-client -y
 clear
 
-
+# Install Squid
+apt-get -y install squid3
+cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/Theeraphong045/th/master/squid.conf" 
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
+sed -i s/xxxxxxxxx/$MYIP/g /etc/squid3/squid.conf;
+service squid3 restart
+clear
 
 # Enable Firewall
 sudo ufw allow 22,80,81,222,443,8080,9700,60000/tcp
